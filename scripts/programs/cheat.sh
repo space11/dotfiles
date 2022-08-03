22d849ddfa4c9e9b312f8287267c00eb50ffe956
Borys Olejniczak <borys.olejniczak@gmail.com>
2022-08-03 11:59:32 +0100

    Add cheat.sh local instalation.

---
+++ scripts/programs/cheat.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# https://github.com/chubin/cheat.sh
+# Install globalky for all users
+curl -s https://cht.sh/:cht.sh | sudo tee /usr/local/bin/cht.sh && sudo chmod +x /usr/local/bin/cht.sh
