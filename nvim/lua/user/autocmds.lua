57c26fc27c91cfb9b2bad5134268c6324d357310
space11 <borys.olejniczak@gmail.com>
2024-02-22 07:55:11 +0000

    fix: text yank autocmd

--- nvim/lua/user/autocmds.lua
+++ nvim/lua/user/autocmds.lua
@@ -47,10 +47,14 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
   end,
 })
 
-vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+-- Highlight on yank
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+  pattern = "*",
   callback = function()
-    vim.highlight.on_yank { higroup = "Visual", timeout = 500 }
+    vim.highlight.on_yank()
   end,
+  group = highlight_group,
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
